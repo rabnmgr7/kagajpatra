d1ef16aa909f3fee21f278b648096fa786473fb0
rabnmgr7 <rabnmgr7@gmail.com>
2024-01-19 11:53:31 +0000

    final build

--- image-build.sh
+++ image-build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo "Removing previous buit image:"
+echo "Checking for previously running services..."
+docker service ls
+echo "Removing Running kagajpatra services and network if running..."
+docker service rm kagajpatra_web kagajpatra_db kagajpatra_nginx || true
+docker network rm -f kagajpatra_server_network
+echo "Removing previous previously built images..."
 docker image rm -f kagajpatra_web:latest kagajpatra_nginx:latest || true
 
-echo "Composing docker compose file"
+echo "Building images from Dockerfiles..."
 docker image build -t kagajpatra_web:latest -f Dockerfile-web .
 docker image build -t kagajpatra_nginx:latest -f Dockerfile-nginx .
--- service_create.sh
+++ service_create.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-echo "Checking for previously running services:"
-docker service ls
-
-echo "Removing services and network if running."
-docker service rm kagajpatra_web kagajpatra_db kagajpatra_nginx || true
-docker network rm -f kagajpatra_server_network
-
-echo "Deploying services as stack:"
+echo "Deploying services as stack..."
 docker stack deploy -c docker-stack.yaml kagajpatra
 
-echo "Done! showing running services:"
+echo "Done!"
 docker service ls
