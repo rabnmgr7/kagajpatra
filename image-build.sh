e1349a01ec5332763ac4f10d4c93269e65eb0106
rabnmgr7 <rabnmgr7@gmail.com>
2024-01-25 15:07:27 +0545

    docker images line added

--- image-build.sh
+++ image-build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-
+echo "Building Docker images!!"
 docker image build -t harbor.registry.local/kagajpatra/web:$BUILD_NUMBER -f Dockerfile-web .
 
 docker image build -t harbor.registry.local/kagajpatra/nginx:$BUILD_NUMBER -f Dockerfile-nginx .
 
 docker image tag mysql:latest harbor.registry.local/kagajpatra/mysql:$BUILD_NUMBER
 
+echo "Showing images!!"
+docker images
+
+echo "Pushing Docker images to Harbor repository!!"
 docker image push harbor.registry.local/kagajpatra/web:$BUILD_NUMBER || true
 
 docker image push harbor.registry.local/kagajpatra/nginx:$BUILD_NUMBER || true
