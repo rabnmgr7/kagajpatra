4cfcb4e2edd5e27a82ab9fb9513949a9f5f099a5
rabnmgr7 <rabnmgr7@gmail.com>
2024-01-25 14:45:10 +0545

    Jenkinsfile added

--- container-create.sh
+++ container-create.sh
@@ -9,8 +9,6 @@ docker container rm kagajpatra_nginx || true
 docker container rm kagajpatra_mysql || true
 
 echo "Creating new container"
-docker run -itd --name kagajpatra-web -p 5000:5000 harbor.registry.local/kagajpatra/web:$BUILD_NUMBER
-
-docker run -itd --name kagajpatra-mysql -p 3306:3306 harbor.registry.local/kagajpatra/mysql:$BUILD_NUMBER
-
-docker run -itd --name kagajpatra-nginx -p 8200:80 -v ./nginx/nginx.conf:/etc/nginx/nginx.conf -v ./app/static:/kagajpatra/app/static ./app/uploads:/kagajpatra/app/uploads harbor.registry.local/kagajpatra/nginx:$BUILD_NUMBER
+docker run -itd --name kagajpatra_web -p 5000:5000 harbor.registry.local/kagajpatra/web:$BUILD_NUMBER
+docker run -itd --name kagajpatra_mysql -p 3306:3306 harbor.registry.local/kagajpatra/web:$BUILD_NUMBER
+docker run -itd --name kagajpatra_nginx -p 8200:80 harbor.registry.local/kagajpatra/web:$BUILD_NUMBER
