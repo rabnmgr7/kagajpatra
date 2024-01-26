a3514df38555c0d5aeb1f1d8de5ad87bceb38588
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-01-27 00:57:45 +0545

    Update image-build.sh

--- image-build.sh
+++ image-build.sh
@@ -10,6 +10,8 @@ docker image tag mysql:latest $REGISTRY/kagajpatra/mysql:$BUILD_NUMBER
 echo "Showing images!!"
 docker images
 
+echo "Logging in to Harbor Registry"
+echo "Rabin@123" | docker login -u "admin" --password-stdin $REGISTRY > /dev/null
 echo "Pushing Docker images to Harbor repository!!"
 docker image push $REGISTRY/kagajpatra/web:$BUILD_NUMBER || true
 docker image push $REGISTRY/kagajpatra/nginx:$BUILD_NUMBER || true
