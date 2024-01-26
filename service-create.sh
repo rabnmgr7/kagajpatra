08b7d14da9d7b33c83608fe1e8d056c06b4987c8
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-01-27 00:51:31 +0545

    Update service-create.sh

--- service-create.sh
+++ service-create.sh
@@ -3,7 +3,6 @@
 docker compose down || true
 echo "Logging in to harbor"
 echo "Rabin@123" | docker login -u "admin" --password-stdin harbor.registry.local > /dev/null
-docker compose down || true
 echo "Deploying services"
 # Starting new services
 docker compose up -d
