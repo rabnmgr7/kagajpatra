721f9ff28aca71891064eb55ca09f945ace97981
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-01-27 00:24:10 +0545

    Update service-create.sh

--- service-create.sh
+++ service-create.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #Removing previously ran services
-echo "Logging into Harbor registry"
-docker login harbor.registry.local || true
 echo "Deploying services"
 docker compose down || true
 # Starting new services
