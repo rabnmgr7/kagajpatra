a643a44f0e8d4d7f3ff79b43b2b2b9e14a0df27e
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-01-26 22:42:08 +0545

    Update image-build.sh

--- image-build.sh
+++ image-build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+REGISTRY='harbor.registry.local'
 echo "Building Docker images!!"
-docker image build -t harbor.registry.local/kagajpatra/web:$BUILD_NUMBER -f Dockerfile-web .
+docker image build -t $REGISTRY/kagajpatra/web:$BUILD_NUMBER -f Dockerfile-web .
 
-docker image build -t harbor.registry.local/kagajpatra/nginx:$BUILD_NUMBER -f Dockerfile-nginx .
+docker image build -t $REGISTRY/kagajpatra/nginx:$BUILD_NUMBER -f Dockerfile-nginx .
 
-docker image tag mysql:latest harbor.registry.local/kagajpatra/mysql:$BUILD_NUMBER
+docker image tag mysql:latest $REGISTRY/kagajpatra/mysql:$BUILD_NUMBER
 
 echo "Showing images!!"
 docker images
 
 echo "Pushing Docker images to Harbor repository!!"
-docker image push harbor.registry.local/kagajpatra/web:$BUILD_NUMBER || true
-docker image push harbor.registry.local/kagajpatra/nginx:$BUILD_NUMBER || true
-docker image push harbor.registry.local/kagajpatra/mysql:$BUILD_NUMBER || true
+docker image push $REGISTRY/kagajpatra/web:$BUILD_NUMBER || true
+docker image push $REGISTRY/kagajpatra/nginx:$BUILD_NUMBER || true
+docker image push $REGISTRY/kagajpatra/mysql:$BUILD_NUMBER || true
