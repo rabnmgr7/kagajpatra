9abb778ee2f8033b958afe51d164acf08bde82df
rabnmgr7 <rabnmgr7@gmail.com>
2024-01-27 01:07:21 +0545

    secrets

--- .backups/service_create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Showing previous services:"
-docker service ls
-echo "Removing previous service if present:"
-docker service rm kagajpatra_web kagajpatra_db kagajpatra_nginx || true
-echo "Deploying services as stack"
-docker stack deploy -c docker-compose.yaml kagajpatra
-echo "Done!"
--- service-create.sh
+++ service-create.sh
@@ -2,7 +2,7 @@
 #Removing previously ran services
 docker compose down || true
 echo "Logging in to harbor"
-echo "Rabin@123" | docker login -u "admin" --password-stdin harbor.registry.local > /dev/null
+cat ./.secrets/.password | docker login -u "admin" --password-stdin harbor.registry.local > /dev/null
 echo "Deploying services"
 # Starting new services
 docker compose up -d
