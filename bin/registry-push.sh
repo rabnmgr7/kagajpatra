d7836af1c23d6b33de1eba64ace0bbade952b335
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-01-28 23:17:04 +0545

    Update registry-push.sh

--- bin/registry-push.sh
+++ bin/registry-push.sh
@@ -2,15 +2,15 @@
 REGISTRY="harbor.registry.local"
 echo "Showing images!!"
 docker images | grep -i "kagajpatra"
-docker image tag $REGISTRY/kagajpatra/web:$BUILD_NUMBER $REGISTRY/kagajpatra/web:latest
+docker image tag $REGISTRY/kagajpatra/web:$BUILD_NUMBER $REGISTRY/kagajpatra/web_v1:$BUILD_NUMBER
 
-docker image tag $REGISTRY/kagajpatra/nginx:$BUILD_NUMBER $REGISTRY/kagajpatra/nginx:latest
+docker image tag $REGISTRY/kagajpatra/nginx:$BUILD_NUMBER $REGISTRY/kagajpatra/nginx_v1:$BUILD_NUMBER
 
-docker image tag $REGISTRY/kagajpatra/mysql:$BUILD_NUMBER $REGISTRY/kagajpatra/mysql:latest
+docker image tag $REGISTRY/kagajpatra/mysql:$BUILD_NUMBER $REGISTRY/kagajpatra/mysql_v1:$BUILD_NUMBER
 
 echo "Logging in to Harbor Registry"
-echo "Rabin@123" | docker login -u "admin" --password-stdin $REGISTRY > /dev/null
+cat ./.secrets/.password | docker login -u "admin" --password-stdin $REGISTRY > /dev/null
 echo "Pushing Docker images to Harbor repository!!"
-docker image push $REGISTRY/kagajpatra/web:latest || true
-docker image push $REGISTRY/kagajpatra/nginx:latest || true
-docker image push $REGISTRY/kagajpatra/mysql:latest || true
+docker image push $REGISTRY/kagajpatra/web_v1:$BUILD_NUMBER || true
+docker image push $REGISTRY/kagajpatra/nginx_v1:$BUILD_NUMBER || true
+docker image push $REGISTRY/kagajpatra/mysql_v1:$BUILD_NUMBER || true
