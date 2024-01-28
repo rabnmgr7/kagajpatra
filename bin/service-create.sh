930d0c785013627119e5136f347e195523a0c9c5
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-01-28 22:47:29 +0545

    Update service-create.sh

--- bin/service-create.sh
+++ bin/service-create.sh
@@ -3,6 +3,7 @@ echo "Which user is running these commands!"
 whoami
 #Removing previously ran services
 docker compose down || true
+echo 'y' | docker network prune
 echo "Logging in to harbor"
 cat ./.secrets/.password | docker login -u "admin" --password-stdin harbor.registry.local > /dev/null
 echo "Deploying services"
