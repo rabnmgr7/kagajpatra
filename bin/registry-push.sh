17d842d5f0d70a0087068ff7c25466a092c36211
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-01-28 22:32:57 +0545

    Update registry-push.sh

--- bin/registry-push.sh
+++ bin/registry-push.sh
@@ -11,6 +11,6 @@ docker image tag $REGISTRY/kagajpatra/mysql:$BUILD_NUMBER $REGISTRY/kagajpatra/m
 echo "Logging in to Harbor Registry"
 echo "Rabin@123" | docker login -u "admin" --password-stdin $REGISTRY > /dev/null
 echo "Pushing Docker images to Harbor repository!!"
-docker image push $REGISTRY/kagajpatra/web:v1 || true
-docker image push $REGISTRY/kagajpatra/nginx:v1 || true
-docker image push $REGISTRY/kagajpatra/mysql:v1 || true
+docker image push $REGISTRY/kagajpatra/web:latest || true
+docker image push $REGISTRY/kagajpatra/nginx:latest || true
+docker image push $REGISTRY/kagajpatra/mysql:latest || true
